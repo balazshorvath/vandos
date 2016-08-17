ff4b756c361b1d543fa7ca6ac153cfcc58c1ea1e
balazshorvath <balazs.peter.horvath@gmail.com>
2016-08-17 22:55:50 +0200

    The complete bootloader v2. Doesnt work yet, stops at loading the Root directory. Probably some miscalculation or setup in FS.

--- boot_v2.asm
+++ boot_v2.asm
@@ -55,6 +55,8 @@ main:
 	MOV AX, 07C0h	; DS should be 07C0h
 	MOV DS, AX
 	MOV ES, AX
+	; Init (0)
+	CALL stage_passed
 	
 	;----------------------------------
 
@@ -63,8 +65,8 @@ main:
 	; NOTE: A few early BIOSes are reported to improperly set DL
 
 	CMP DL, 0
-	JE no_change
-	MOV [bootdev], DL			; Save boot device number
+	JE .no_change
+	MOV [boot_device], DL			; Save boot device number
 	
 	MOV AH, 8					; Get drive parameters
 	INT 13h
@@ -77,11 +79,13 @@ main:
 	ADD DX, 1					; Head numbers start at 0 - add 1 for total
 	MOV [heads_per_cylinder], DX
 
-no_change:
+.no_change:
 	XOR EAX, EAX				; Needed for some older BIOSes
 	
 	; END OF COPY
 	;----------------------------------
+	; Init device data (1)
+	CALL stage_passed
 	
 	MOV AX, 19					; Root dir starts at logical sector 19
 	CALL lba_to_chs
@@ -89,28 +93,163 @@ no_change:
 	MOV BX, BUFFER
 	MOV AH, 2					; Function
 	MOV AL, 14					; Amount
-	
+								; 244 entries 32 bytes each
+
 	MOV CX, 5
 	
-read_root:
+.read_root:
 	PUSHA
 	
 	STC
 	INT 13h
-	JNC search_stage_two		; If CF not set, we're good
+	JNC .search_stage_two		; If CF not set, we're good
 	
 	XOR AX, AX					; Reset
 	INT 13h
 	
 	POPA
-	LOOP read_root
+	LOOP .read_root
 	JMP boot_failure
-search_stage_two:
+.search_stage_two:
+	; Load root dir into memory (2)
+	CALL stage_passed
+	
 	POPA
 
+	MOV DI, BUFFER				; ES is already set properly
+	
+	MOV CX, [root_entries]
+	XOR AX, AX
+
+.check_entry:
+	PUSH CX
+	
+	MOV SI, stage_two_name
+	MOV CX, 11					; Set filename and test for 11 bytes
+	
+	REP CMPSB
+	JE .found_stage_two
+	
+	ADD AX, 32
+	MOV DI, BUFFER
+	ADD DI, AX
+	
+	POP CX
+	LOOP .check_entry
+	JMP boot_failure
+	
+.found_stage_two:
+	; Found stage two file (3)
+	CALL stage_passed
+	
+	POP CX
+	MOV AX, WORD [ES:DI + 0Fh]
+	MOV WORD [cluster], AX
+	
+	MOV AX, 1					; Now load first FAT
+	CALL lba_to_chs
+	
+	MOV AH, 2					; Function
+	MOV AL, [sectors_per_fat]	; Amount
+	MOV CX, 5
+	
+.read_fat:
+	PUSHA
+	
+	STC
+	INT 13h
+	JNC .load_stage_two			; If CF not set, we're good
+	
+	XOR AX, AX					; Reset
+	INT 13h
+	
+	POPA
+	LOOP .read_fat
+	JMP boot_failure
+.load_stage_two:
+	; FAT is in memory (4)
+	CALL stage_passed
+	
+	POPA
+	; Buffer + (9 * 512) is the place to load the second stage
+	; We dont want to override anything
+	; Starting from offset 0
+	MOVZX AX, [sectors_per_fat]
+	MUL WORD [bytes_per_sector]
+	ADD AX, BUFFER
+	MOV ES, AX
+	
+	XOR BX, BX
+	MOV WORD [pointer], BX			; Just to be sure
+	
+	MOV AH, 02h						; Function
+	MOV AL, [sectors_per_cluster]	; Amount
+	PUSH AX							; Will be used later
+	
+.read_cluster:
+	MOV AX, WORD [cluster]
+	ADD AX, 31						; Cluster to LBA
+	CALL lba_to_chs
+	
+	MOV BX, WORD [pointer]
+	
+	POP AX
+	PUSH AX
+	
+	STC
+	INT 13h
+	JNC .next_cluster
+	
+	XOR AX, AX					; Reset
+	INT 13h
+	; TODO: MAX TRIES
+	JMP .read_cluster
+	
+.next_cluster:
+	MOV AX, [cluster]
+	MOV BX, 3
+	MUL BX
+	SHR AX, 1			; 3*cluster/2
+	
+	MOV SI, BUFFER
+	ADD SI, AX
+	MOV DX, WORD [DS:SI]
+	; If cluster is even, drop last 4 bits of word
+	; with next cluster; if odd, drop first 4 bits
+	AND AX, 1
+	JZ .even
+	
+	SHR DX, 4
+	JMP SHORT .next_cluster_cont
+.even:
+	AND DX, 0FFFh
+.next_cluster_cont:
+	MOV WORD [cluster], DX
+	CMP DX, 0FF8h		; Means, its EOF (FAT12)
+	JAE finished
+	
+	MOVZX AX, [sectors_per_cluster]
+	MUL WORD [bytes_per_sector]
+	ADD WORD [pointer], AX
+	JMP .read_cluster
+	
+	
+finished:
+	; File loaded (5)
+	CALL stage_passed
+	
+	MOVZX AX, [sectors_per_fat]
+	MUL WORD [bytes_per_sector]
+	ADD AX, BUFFER
+	
+	PUSH AX
+	PUSH 0
+	RETF	; RETF : Pops IP, then CS
+	
+	
 ; ------------------------------------------------------------------
 ; lba_to_chs
-; IN:	AX		- LBA addresss
+; IN:	AX		- LBA
 ;
 ; OUT:	DL 		- Sector
 ;		DH		- Head
@@ -135,8 +274,23 @@ lba_to_chs:
 	POP AX
 	
 	RET
+
+; ------------------------------------------------------------------
+; stage_passed
+; IN/OUT NOTHING
+; Prints a number to the console via BIOS call.
+; The number is increased every time its called.
+; Max calls: 9, after it will be invalid mnumber.
+; ------------------------------------------------------------------
+stage_passed:
+	MOV AL, [stage_count]
+	MOV AH, 0Eh
+	INT 10h
 	
-	
+	INC AL
+	MOV [stage_count], AL
+	RET
+
 boot_failure:
 	MOV AH, 0
 	INT 16h				; Wait for key
@@ -148,18 +302,14 @@ boot_failure:
 
 BUFFER							equ	0BC0h
 
+stage_count						db	30h	;0
 boot_device						db	0
 cluster							dw	0
-pointerdw						dw	0
+pointer							dw	0
 
 stage_two_name 					db	"STGTWO  BIN"
 	
-msg_started 					db	"S1", 10, 13, 0
-
-msg_root_dir 					db	"RD", 10, 13, 0
-msg_fat	 						db	"FAT", 10, 13, 0
-msg_stage_two					db	"LS2", 10, 13, 0
-msg_find_stage_two				db	"FS2", 10, 13, 0
+msg_started 					db	"ERROR", 10, 13, 0
 
 msg_finished 					db	"Done", 10, 13, 0
 
